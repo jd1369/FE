c361d1bde5501da6f4de011e7153fb0167402d65
jd1369 <51992032+jd1369@users.noreply.github.com>
2024-12-06 13:43:32 +0530

    as

--- src/app/auth/auth.interceptor.ts
+++ src/app/auth/auth.interceptor.ts
@@ -1,64 +1,21 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Router } from '@angular/router';
-import { BehaviorSubject } from 'rxjs';
-import { tap } from 'rxjs/operators';
-import { environment } from 'src/environments/environment';
-@Injectable({
-  providedIn: 'root',
-})
-export class AuthService {
-  private apiURL = 'http://localhost:1369';
-  baseUrl =environment.baseUrl;
-  private tokenKey = 'authToken';
-  private isAuthenticated$ = new BehaviorSubject<boolean>(this.hasToken());
+import { HttpInterceptor, HttpRequest, HttpHandler } from '@angular/common/http';
+import { AuthService } from './auth.service';
 
-  constructor(private http: HttpClient, private router: Router) {}
+@Injectable()
+export class AuthInterceptor implements HttpInterceptor {
+  constructor(private authService: AuthService) {}
 
-  private hasToken(): boolean {
-    return !!localStorage.getItem(this.tokenKey);
-  }
-
-  isLoggedIn() {
-    return this.isAuthenticated$.asObservable();
-  }
-
-
-
-
-  
-  getOtp(phoneNumber:string){
-    return this.http
-    .post(`${this.baseUrl}generateOtp/`+phoneNumber, { phoneNumber })
-  }
-
-  login(phoneNumber: string, otp: string) {
-    return this.http
-      .post<{ token: string }>(`${this.baseUrl}login`, { phoneNumber, otp })
-      .pipe(
-        tap((response) => {
-          localStorage.setItem(this.tokenKey, response.token);
-          this.isAuthenticated$.next(true);
-        })
-      );
-  }
-
-  loginWithtoken(token:string){
-    return this.http.post(`${this.baseUrl}`+'loginWithToken',{token})
-  }
-
-
-  register(emailId: string, name: string, phoneNumber: string) {
-    return this.http.post(`${this.apiURL}/register`, { name, phoneNumber, emailId });
-  }
-
-  logout() {
-    localStorage.removeItem(this.tokenKey);
-    this.isAuthenticated$.next(false);
-    this.router.navigate(['/base/home']);
-  }
-
-  getToken() {
-    return localStorage.getItem(this.tokenKey);
+  intercept(req: HttpRequest<any>, next: HttpHandler) {
+    const token = this.authService.getToken();
+    if (token) {
+      const cloned = req.clone({
+        setHeaders: {
+          Authorization: `Bearer ${token}` // Add the Bearer token
+        }
+      });
+      return next.handle(cloned);
+    }
+    return next.handle(req);
   }
 }
